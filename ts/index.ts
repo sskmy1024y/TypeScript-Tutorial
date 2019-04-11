97a9633b62451c199b56af63b652197c43ad6978
sho <sskmy1024.y@gmail.com>
2019-04-11 11:21:40 +0900

    use package and element click function

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,17 @@
+import UUID from 'uuid';
 import { User } from './User';
+
 
 const user = new User('海老原', '賢次', 44); // 名前と年齢は適当に
 
 const contentsElem = document.getElementById('contents');
 if(!!contentsElem) {
   contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+}
+
+const saibanButton = document.getElementById('saiban') as HTMLButtonElement;
+const uuidSpan = document.getElementById('uuid') as HTMLSpanElement;
+
+saibanButton.onclick = (e) => {
+  uuidSpan.innerText = UUID.v4();
 }
